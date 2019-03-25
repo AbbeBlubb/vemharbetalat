27567051c9fff2b68037d6590c3c65237fbe5589
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-03-26 00:25:45 +0100

    Routing, PageNoMatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import React          from 'react';
-import {BrowserRouter, Route, Link, Redirect, withRouter} from "react-router-dom";
-import firebase           from '../config/firebase'
-import {MainFrame}    from './MainFrame';
-import {PageStart}    from './views/PageStart';
-import {PageNewUser} from "./views/PageNewUser";
-import {PageAuthenticated} from "./views/PageAuthenticated";
+import React                from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  withRouter}               from "react-router-dom";
+import firebase             from '../config/firebase'
+import {MainFrame}          from './MainFrame';
+import {PageStart}          from './views/PageStart';
+import {PageNewUser}        from "./views/PageNewUser";
+import {PageAuthenticated}  from "./views/PageAuthenticated";
+import {PageNoMatch}        from "./views/PageNoMatch";
 import './helpers/InteractionHelper';
 import './helpers/waves'
 
@@ -38,11 +45,15 @@ export default class App extends React.Component {
   render() {
     return (
       <MainFrame>
-        <BrowserRouter>
-          <Route path='/'         component={PageStart}         exact />
-          <Route path='/new'      component={PageNewUser}             />
-          <Route path='/account'  component={PageAuthenticated}       />
-        </BrowserRouter>
+        <Router>
+          {/* Switch renders only the first match, thus the possibility for the NoMatch component */}
+          <Switch>
+            <Route exact  path='/'        component={PageStart}         />
+            <Route        path='/new'     component={PageNewUser}       />
+            <Route        path='/account' component={PageAuthenticated} />
+            <Route                        component={PageNoMatch}       />
+          </Switch>
+        </Router>
       </MainFrame>
     );
   }
---
+++ src/components/views/PageNoMatch.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export function PageNoMatch() {
+  return (
+      <div className='padding-top-bottom-max-height-650px'>
+        <section className='page-no-match'>
+          <span className='page-no-match__message'>
+            404
+          </span>
+        </section>
+      </div>
+  )
+}
