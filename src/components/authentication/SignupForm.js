13d37a0c0778839af19af6481e7cfc0c61045782
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-12-08 03:10:41 +0100

    SignupForm

--- src/components/authentication/SignupForm.js
+++ src/components/authentication/SignupForm.js
@@ -5,33 +5,66 @@ export class SignupForm extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      test: ''
+      username: '',
+      password: '',
+      error: null
     };
   }
 
-  handleSignup = () => {
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSignup = event => {
+    event.preventDefault();
     fetch('https://cyberwall.herokuapp.com/signup', {
       method: 'POST',
+      //mode: 'no-cors',
       // Uploading JSON data, can be string or obj (the api expects an object)
-      body: '',
-      headers: { 'Content-Type': 'application/json'}
+      body: JSON.stringify({ email: this.state.username, password: this.state.password }),
+      headers: { 'Content-Type': 'application/json' }
     })
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log('Error: ',error));
+      .then(response => {
+        if (!response.ok) {
+          const body = response.json();
+          console.log(body)
+          const message = Object.keys(body)[0];
+          this.setState({ error: message });
+          throw Error(response.statusText);
+        } else {
+          return response.json();
+        }
+      })
+      .then(json => {console.log(json)})
+      // If the promise is rejected by network or by the throw Error
+      .catch(error => console.log(error));
   };
 
   render() {
     return (
       <>
         {/* The reduxForm makes props.handleSubmit available, as arg the function to be called, as reference, not calling() */}
-        <form onSubmit={() => {}}>
+        <form onSubmit={event => this.handleSignup(event)}>
 
           <label htmlFor='email'>Användarnamn</label>
-          <input type='text' />
+          <input
+            type='text'
+            name='username'
+            placeholser='Användarnamn'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
 
           <label htmlFor='password'>Lösenord</label>
-          <input type='password' />
+          <input
+            type='password'
+            name='password'
+            placeholser='Lösenord'
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          {this.state.error && <div>{this.state.error.message}</div>}
 
           <button type='submit'>Register</button>
         </form>
