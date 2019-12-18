4c15ab0cc56d383fc5ee0d3eb09ac415c2a32870
AbbeBlubb <31575083+AbbeBlubb@users.noreply.github.com>
2019-12-18 01:02:18 +0100

    UserList fetch, UserList styling

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,70 +6,45 @@ class UserList extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      userNames: [
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'Chefen',
-        'Snorkfröken',
-        'kudden',
-        'pepparkakan',
-        'Cybertruck',
-        'hej'
-      ]
+      users: []
     };
   }
 
+  fetchUsers = (fetch, URL) => {
+    fetch(URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `JWT ${sessionStorage.getItem('token')}`
+      }
+    })
+    // Has to either return; inside {}, or => response.json() without {} nor .json();
+    .then(response => response.json())
+    .then(data => this.setState({ users: data }));
+  }
+
   componentDidMount() {
-    this.props.numberOfUsersToState(this.state.userNames.length);
+    this.fetchUsers(
+      fetch,
+      'https://cyberwall.herokuapp.com/users'
+    );
+
+    //this.props.numberOfUsersToState(this.state.userNames.length);
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state !== prevState) {
-      this.props.numberOfUsersToState(this.state.userNames.length);
+      this.props.numberOfUsersToState(this.state.users.length);
     }
   }
 
   render() {
     return (
       <ul className={'user-list'}>
-        {this.state.userNames.map((user, index) => (
-          <li
-            key={index}
-          >
-            {user}
+        {this.state.users.map((user, index) => (
+          <li key={index} className={'user-list__item'}>
+            <span className={'user-list__username'}>{user.username}</span>
+            <span className={'user-list__date'}>{user.date.slice(2, 10)}</span>
           </li>
         ))}
       </ul>
